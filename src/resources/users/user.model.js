3ed2903734e3713c6ef560f2cce6a1b60199bd3f
AlexeyParfimovich <alexey.parfimovich@yandex.by>
2021-05-26 18:34:41 +0300

    feat: implement jsdocs for User model

--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,28 +1,60 @@
 const uuid = require('uuid').v1;
 
+/**
+ * Class to create a User object
+ */
 class User {
+  /**
+   * @param {{}} [userInfo] User information obtained from the request 
+   */
   constructor({
     id = uuid(),
     name = 'Default User',
     login = 'user',
     password = 'P@ssw0rd'
   } = {}) {
+    /** 
+     * User identifier
+     * @type {string}
+     */
     this.id = id;
+    /** 
+     * User name 
+     * @type {string}
+     */
     this.name = name;
+    /** 
+     * User login 
+     * @type {string}
+     */
     this.login = login;
+    /** 
+     * User password 
+     * @type {string}
+     */
     this.password = password;
-  }
+  };
 
-  // Фильтрация вывода свойств объекта
+  /**
+   * Static method to filter off the password field
+   * @param {{}} user 
+   * @returns {{}}
+   * @static
+   */
   static toRes(user) {
     const { id, name, login } = user;
     return { id, name, login };
-  }
+  };
 
-  // Создать и вернуть новый объект
+  /**
+   * Static method to create, initiate and return new User
+   * @param {Object} body 
+   * @returns {Object}
+   * @static
+   */
   static fromReq(body) {
     return new User(body);
-  }
+  };
   
 }
 
