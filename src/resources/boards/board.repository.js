f27e7da3dadfa8e0cc587dd41955cfe52720c94d
AlexeyParfimovich <alexey.parfimovich@yandex.by>
2021-05-28 18:17:45 +0300

    refactor: update jsdoc for boards resources

--- src/resources/boards/board.repository.js
+++ src/resources/boards/board.repository.js
@@ -1,12 +1,31 @@
-const dataBase = require('../../utils/inMemoryDb');
+/**
+ * Board repository
+ * @module boardRepository
+ */
 
+const dataBase = require('../../utils/inMemoryDb');
 const { NOT_FOUND_ERROR } = require('../../errors/httpError404');
 
+/**
+ * @const {string}
+ */
 const TABLE_NAME = 'Boards';
+/**
+ * @const {string}
+ */
 const ENTITY_NAME = 'board';
 
+/**
+ * Function to get all entities from Boards table
+ * @returns {Promise<Array.<Object>>} Array of all objects from the table
+ */
 const getAll = async () => dataBase.getAllEntities(TABLE_NAME);;
 
+/**
+ * Function to get all an entity from Boards table by specified identifier
+ * @param {string} id - Board identifier
+ * @returns {Promise<Object.<string,string>>} An object selected by id
+ */
 const getById = async (id) => {
   const item = await dataBase.getEntityById(TABLE_NAME, id);
   if (!item) {
@@ -15,8 +34,19 @@ const getById = async (id) => {
   return item;
 };
 
+/**
+ * Function for adding an entity into the Boards table
+ * @param {Object.<string, string>} entity - Object for adding
+ * @returns {Promise<Object.<string,string>>} An object added to the table
+ */
 const addEntity = async (entity) => dataBase.addEntity(TABLE_NAME, entity);
 
+/**
+ * Function for updating an entity in the Boards table by specified identifier
+ * @param {string} id - Board identifier
+ * @param {Object.<string, string>} entity - Object for updating
+ * @returns {Promise<Object.<string,string>>} An object updated in the table
+ */
 const updateById = async (id, entity) => {
   const item = await dataBase.updateEntity(TABLE_NAME, id, entity);
   if (!item) {
@@ -25,6 +55,11 @@ const updateById = async (id, entity) => {
   return item;
 };
 
+/**
+ * Function to deleting all an entity from Boards table by specified identifier
+ * @param {string} id - Board identifier
+ * @returns {Promise<void>}
+ */
 const deleteById = async (id) => {
   if (! await dataBase.deleteEntity(TABLE_NAME, id)) {
     throw new NOT_FOUND_ERROR(`Couldn't find a ${ENTITY_NAME} with ID:${id} `);
--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -1,22 +1,47 @@
+/**
+ * Board service
+ * @module boardService
+ */
+
 const boards = require('./board.repository');
 const deleteAllTasks = require('../tasks/task.service').deleteByBoard;
 
+/**
+ * Function to get all entities from Boards table
+ * @returns {Promise<Array.<Object.<string,string>>>} Array of boards
+ */
 const getAll = () => boards.getAll();
 
+/**
+ * Function to get all an entity from Boards table by specified identifier
+ * @param {string} id - Board identifier
+ * @returns {Promise<Object.<string,string>>} A board object
+ */
 const getById = (id) => boards.getById(id);
 
-const findById = (id) => boards.findById(id);
-
-/*
-  TODO: When somebody DELETEs Board, all its Tasks should be deleted as well.
-*/
+/**
+ * Function to delete an entity from Boards table by specified identifier
+ * @param {string} id - Board identifier
+ * @returns {Promise<void>}
+ */
 const deleteById = async (id) => {
   await boards.deleteById(id);
   await deleteAllTasks(id);
 };
 
+/**
+ * Function for adding an entity into the Boards table
+ * @param {Object.<string, string>} entity - Object for adding
+ * @returns {Promise<Object.<string,string>>} A board object added
+ */
 const addEntity = (entity) => boards.addEntity(entity);
 
+/**
+ * Function for updating an entity in the Boards table by specified identifier
+ * @param {string} id - Board identifier
+ * @param {Object.<string, string>} entity - Object for updating
+ * @returns {Promise<Object.<string,string>>} A board object updated
+ */
 const updateById = (id, entity) => boards.updateById(id, entity);
 
-module.exports = { getAll, getById, findById, addEntity, updateById, deleteById };
+module.exports = { getAll, getById, addEntity, updateById, deleteById };
